6c6101b81710e4024acd9b69899d49535edc5ae1
Martossy Alex <github@mail.martossy.hu>
2024-03-24 11:41:10 +0100

    Fix dark mode contrast

--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -18,7 +18,7 @@ export default function Footer() {
                 href="https://www.facebook.com/Magyarorsz%C3%A1g-%C3%9Cgy%C3%A9szs%C3%A9ge-104594918109266"
                 rel="noreferrer"
                 target="_blank"
-                className="text-primary transition hover:opacity-75"
+                className="text-primary transition hover:opacity-75 dark:text-bg-contrast"
               >
                 <span className="sr-only">Facebook</span>
 
@@ -31,7 +31,7 @@ export default function Footer() {
                 href="https://www.youtube.com/@MagyarorszagUgyeszsege"
                 rel="noreferrer"
                 target="_blank"
-                className="text-primary transition hover:opacity-75"
+                className="text-primary transition hover:opacity-75 dark:text-bg-contrast"
               >
                 <span className="sr-only">YouTube</span>
                 <FontAwesomeIcon icon={faYoutube} height={24} width={24} />
@@ -43,7 +43,7 @@ export default function Footer() {
                 href="https://twitter.com/ProsecutionHu"
                 rel="noreferrer"
                 target="_blank"
-                className="text-primary transition hover:opacity-75"
+                className="text-primary transition hover:opacity-75 dark:text-bg-contrast"
               >
                 <span className="sr-only">X</span>
 
@@ -56,7 +56,7 @@ export default function Footer() {
                 href="https://github.com/alexaka1/mhu-nextjs-sport"
                 rel="noreferrer"
                 target="_blank"
-                className="text-primary transition hover:opacity-75"
+                className="text-primary transition hover:opacity-75 dark:text-bg-contrast"
               >
                 <span className="sr-only">GitHub</span>
 
