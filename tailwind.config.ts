0dd3bbeee16839e40069a343021c3d2a109935c3
Martossy Alex <github@mail.martossy.hu>
2024-03-24 08:36:03 +0100

    Added color theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,33 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          default: 'oklch(33.14% 0.1186 18.15 / <alpha-value>)',
+          800: 'oklch(33.14% 0.1186 18.15 / <alpha-value>)',
+          600: 'oklch(33.14% 0.1186 18.15 / <alpha-value>)',
+          400: 'oklch(33.14% 0.1186 18.15 / <alpha-value>)',
+          200: 'oklch(33.14% 0.1186 18.15 / <alpha-value>)',
+        },
+        hun: {
+          red: 'oklch(33.14% 0.1186 18.15 / <alpha-value>)',
+          green: 'oklch(48.74% 0.184 20.24 / <alpha-value>)',
+        },
+        secondary: {
+          default: 'oklch(41.85% 0.1658 25.77 / <alpha-value>)',
+          800: 'oklch(32.5% 0.129 25.77 / <alpha-value>)',
+          600: 'oklch(41.85% 0.1658 25.77 / <alpha-value>)',
+          400: 'oklch(66.5% 0.216 25.77 / <alpha-value>)',
+          200: 'oklch(83.2% 0.085 25.77 / <alpha-value>)',
+        },
+        accent: {
+          default: 'oklch(47.01% 0.1653 18.33 / <alpha-value>)',
+          800: 'oklch(32.5% 0.115 18.33 / <alpha-value>)',
+          600: 'oklch(47.01% 0.1653 18.33 / <alpha-value>)',
+          400: 'oklch(66.5% 0.196 18.33 / <alpha-value>)',
+          200: 'oklch(83.2% 0.077 18.33 / <alpha-value>)',
+        },
+      },
       fontFamily: {
         sans: ['var(--font-geist-sans)', ...fontFamily.sans],
         mono: ['var(--font-geist-mono)', ...fontFamily.mono],
@@ -20,6 +47,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [typography],
+  plugins: [typography, require('tailwindcss-oklch')()],
 };
 export default config;
