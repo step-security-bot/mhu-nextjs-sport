9a4448b0159bc77a0ea9d5846e49856d26be72bf
Alex Martossy <github@mail.martossy.hu>
2024-03-26 21:44:28 +0100

    Update sw.ts

--- src/app/lib/sw.ts
+++ src/app/lib/sw.ts
@@ -18,3 +18,5 @@ installSerwist({
   navigationPreload: true,
   runtimeCaching: defaultCache,
 });
+
+// Put your service worker logic here. https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
