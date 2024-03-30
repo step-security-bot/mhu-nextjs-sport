c025b9a394b01b4a06a622feb0aea88997561c85
Alex Martossy <github@mail.martossy.hu>
2024-03-30 10:09:30 +0100

    Csp review (#23)
    Csp report only, removed nonce

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 import withSerwistInit from '@serwist/next';
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
+// CSP headers here is set based on Next.js recommendations:
+// https://nextjs.org/docs/app/building-your-application/configuring/content-security-policy
+
+/**
+ * @param nonce {string | undefined}
+ * @returns {string}
+ */
+export const createCspHeaders = (nonce) => {
+  const defaultsCSPHeaders = `
+    style-src 'self';
     font-src 'self';
     object-src 'none';
     base-uri 'self';
@@ -12,7 +17,58 @@ const cspHeader = `
     frame-ancestors 'none';
     block-all-mixed-content;
     upgrade-insecure-requests;
-`;
+  `;
+
+  // when environment is preview enable unsafe-inline scripts for vercel preview feedback/comments feature
+  // and whitelist vercel's domains based on:
+  // https://vercel.com/docs/workflow-collaboration/comments/specialized-usage#using-a-content-security-policy
+  // and white-list vitals.vercel-insights
+  // based on: https://vercel.com/docs/speed-insights#content-security-policy
+  if (process.env?.VERCEL_ENV === "preview") {
+    return `
+      ${defaultsCSPHeaders}
+      default-src 'none';
+      script-src 'self' https://vercel.live/ https://vercel.com 'unsafe-inline';
+      connect-src 'self' https://vercel.live/ https://vercel.com https://vitals.vercel-insights.com https://sockjs-mt1.pusher.com/ wss://ws-mt1.pusher.com/;
+      img-src 'self' https://vercel.live/ https://vercel.com https://sockjs-mt1.pusher.com/ data: blob:;
+      frame-src 'self' https://vercel.live/ https://vercel.com;
+      `;
+  }
+
+  // for production environment white-list vitals.vercel-insights
+  // based on: https://vercel.com/docs/speed-insights#content-security-policy
+  if (process.env.NODE_ENV === "production") {
+    // return `
+    //   ${defaultsCSPHeaders}
+    //   default-src 'none';
+    //   script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    //   img-src 'self' blob: data:;
+    //   connect-src 'self' https://vitals.vercel-insights.com;
+    //   `;
+    return `
+      ${defaultsCSPHeaders}
+      default-src 'none';
+      script-src 'self' 'strict-dynamic';
+      img-src 'self' blob: data:;
+      connect-src 'self' https://vitals.vercel-insights.com;
+      `;
+  }
+
+  // for dev environment enable unsafe-eval for hot-reload
+  // return `
+  //   ${defaultsCSPHeaders}
+  //   default-src 'self';
+  //   script-src 'self' 'nonce-${nonce}' 'strict-dynamic' 'unsafe-eval';
+  //   img-src 'self' blob: data:;
+  // `;
+  return `
+    ${defaultsCSPHeaders}
+    default-src 'self';
+    script-src 'self' 'strict-dynamic' 'unsafe-eval';
+    img-src 'self' blob: data:;
+  `;
+}
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -30,8 +86,8 @@ const nextConfig = {
         source: '/(.*)',
         headers: [
           {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ''),
+            key: 'Content-Security-Policy-Report-Only',
+            value: createCspHeaders(undefined).replace(/\n/g, ''),
           },
         ],
       },
