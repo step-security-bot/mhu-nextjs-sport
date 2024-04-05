eba7d8f36e8f5c2ff6dcca2867a85edcb65af973
Martossy Alex <github@mail.martossy.hu>
2024-04-05 21:24:46 +0200

    Allow account relinking with google

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -23,6 +23,7 @@ export const {
     Google({
       clientId: process.env['GOOGLE_CLIENT_ID'] as string,
       clientSecret: process.env['GOOGLE_CLIENT_SECRET'] as string,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
   pages: {
