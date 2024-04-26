24ef6d2f9eded79ec99d6d6b9ffbb1eb1b75c6c6
Alex Martossy <github@mail.martossy.hu>
2024-04-26 12:11:07 +0200

    Bump deps (#51)
    * Update Serwist
    
    * Enable cssnano

--- src/app/lib/sw.ts
+++ src/app/lib/sw.ts
@@ -1,7 +1,6 @@
-import type { SerwistGlobalConfig } from '@serwist/core';
 import { defaultCache } from '@serwist/next/worker';
-import type { PrecacheEntry } from '@serwist/precaching';
-import { Serwist } from '@serwist/sw';
+import type { PrecacheEntry, SerwistGlobalConfig } from 'serwist';
+import { Serwist } from 'serwist';
 
 declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
@@ -11,12 +10,14 @@ declare global {
 
 declare const self: ServiceWorkerGlobalScope;
 
-new Serwist().install({
+const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
   runtimeCaching: defaultCache,
 });
+
+serwist.addEventListeners();
 
 // Put your service worker logic here. https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
