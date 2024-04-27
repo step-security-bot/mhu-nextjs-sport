e48d41e152472f62269c16329cde4dbc44e73af6
Martossy Alex <github@mail.martossy.hu>
2024-04-27 18:49:08 +0200

    Add default slug page instead of redirect for better static generation

---
+++ src/app/(main)/eredmenyek/page.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from 'next';
+import ResultsTab from '@/app/ui/results/results-tab';
+import { Suspense } from 'react';
+import Skeleton from '@/app/ui/skeleton';
+import { Result } from '@/app/lib/types';
+
+export const metadata: Metadata = {
+  title: 'Eredmények',
+};
+
+export default function Page() {
+  const sportag: Result = 'Labdarúgás';
+  return (
+    <main className="flex flex-col items-center justify-center bg-white dark:bg-gray-800">
+      <Suspense key={sportag} fallback={<Skeleton />}>
+        <ResultsTab className={`w-full`} result={sportag} />
+      </Suspense>
+    </main>
+  );
+}
