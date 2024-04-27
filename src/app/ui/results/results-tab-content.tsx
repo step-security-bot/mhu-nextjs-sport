18df5dc1b98d7fd315821f220eadc71a49519c57
Martossy Alex <github@mail.martossy.hu>
2024-04-27 18:42:25 +0200

    Fix iframe fallback

--- src/app/ui/results/results-tab-content.tsx
+++ src/app/ui/results/results-tab-content.tsx
@@ -15,17 +15,9 @@ export default function ResultsContent({ results, canEdit }: Readonly<{ results:
     switch (result.type) {
       case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
         return (
-          <>
-            <ResultsTable
-              key={result.key}
-              file={result.url}
-              canEdit={canEdit}
-              title={result.result}
-              fileKey={result.key}
-            >
-              <XlsxTable fileUrl={result.url} id={result.key} />
-            </ResultsTable>
-          </>
+          <ResultsTable key={result.key} file={result.url} canEdit={canEdit} title={result.result} fileKey={result.key}>
+            <XlsxTable fileUrl={result.url} id={result.key} />
+          </ResultsTable>
         );
       case 'application/pdf': {
         const pdfIframe = new URL(`https://docs.google.com/viewerng/viewer`);
@@ -33,33 +25,25 @@ export default function ResultsContent({ results, canEdit }: Readonly<{ results:
         pdfIframe.searchParams.set('embedded', 'true');
 
         return (
-          <>
-            <ResultsTable
-              key={result.key}
-              file={result.url}
-              canEdit={canEdit}
-              title={result.result}
-              fileKey={result.key}
-            >
-              <object data={result.url} type={result.type} className={`h-dvh w-full`}>
-                <iframe
-                  src={pdfIframe.toString()}
-                  loading={'eager'}
-                  referrerPolicy={'no-referrer'}
-                  className={`h-svh w-full`}
-                  allow={`fullscreen`}
-                >
-                  <p className={`prose text-balance p-2 text-bg-contrast`}>
-                    Ez az eszköz nem támogatja a PDF-ek megjelenítését. Kérjük, töltsd le a PDF-et:{' '}
-                    <a className={`decoration-primary text-primary dark:text-primary-400`} href={result.url}>
-                      PDF letöltése
-                    </a>
-                    .
-                  </p>
-                </iframe>
-              </object>
-            </ResultsTable>
-          </>
+          <ResultsTable key={result.key} file={result.url} canEdit={canEdit} title={result.result} fileKey={result.key}>
+            <object data={result.url} type={result.type} className={`h-dvh w-full`}>
+              <iframe
+                src={pdfIframe.toString()}
+                loading={'eager'}
+                referrerPolicy={'no-referrer'}
+                className={`h-svh w-full`}
+                allow={`fullscreen`}
+              ></iframe>
+              <p className={`prose text-balance p-2 text-bg-contrast`}>
+                Ez az eszköz nem támogatja a PDF-ek megjelenítését. Ha a szöveg felett nem jelent meg semmi, akkor
+                próbáld meg{' '}
+                <a className={`decoration-primary text-primary dark:text-primary-400`} href={result.url}>
+                  letölteni a PDF-et
+                </a>
+                .
+              </p>
+            </object>
+          </ResultsTable>
         );
       }
       default:
