8eb53267af74dd22ce4370b4230df290d5fa2ae8
Alex Martossy <github@mail.martossy.hu>
2024-04-27 16:55:41 +0200

    Android pdf viewer (#53)
    * Add fallback google docs pdf viewer

--- src/app/ui/results/results-tab.tsx
+++ src/app/ui/results/results-tab.tsx
@@ -25,43 +25,62 @@ type Tab = {
 
 function getResults(results: ResultItem[], canEdit?: boolean) {
   const tables = results.map((result) => {
+    if (result.url == null) {
+      return (
+        <p key={result.key} className={`prose text-balance p-2 text-bg-contrast`}>
+          Valami hiba történt.
+        </p>
+      );
+    }
     switch (result.type) {
       case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
         return (
           <>
             <ResultsTable
               key={result.key}
-              file={result.url ?? '#'}
+              file={result.url}
               canEdit={canEdit}
               title={result.result}
               fileKey={result.key}
             >
-              <XlsxTable fileUrl={result.url ?? '#'} id={result.key} />
+              <XlsxTable fileUrl={result.url} id={result.key} />
             </ResultsTable>
           </>
         );
-      case 'application/pdf':
+      case 'application/pdf': {
+        const pdfIframe = new URL(`https://docs.google.com/viewerng/viewer`);
+        pdfIframe.searchParams.set('url', result.url);
+        pdfIframe.searchParams.set('embedded', 'true');
+
         return (
           <>
             <ResultsTable
               key={result.key}
-              file={result.url ?? '#'}
+              file={result.url}
               canEdit={canEdit}
               title={result.result}
               fileKey={result.key}
             >
               <object data={result.url} type={result.type} className={`h-dvh w-full`}>
-                <p className={`prose text-balance p-2 text-bg-contrast`}>
-                  Ez az eszköz nem támogatja a PDF-ek megjelenítését. Kérjük, töltsd le a PDF-et:{' '}
-                  <a className={`decoration-primary text-primary dark:text-primary-400`} href={result.url}>
-                    PDF letöltése
-                  </a>
-                  .
-                </p>
+                <iframe
+                  src={pdfIframe.toString()}
+                  loading={'eager'}
+                  referrerPolicy={'no-referrer'}
+                  className={`h-dvh w-full`}
+                >
+                  <p className={`prose text-balance p-2 text-bg-contrast`}>
+                    Ez az eszköz nem támogatja a PDF-ek megjelenítését. Kérjük, töltsd le a PDF-et:{' '}
+                    <a className={`decoration-primary text-primary dark:text-primary-400`} href={result.url}>
+                      PDF letöltése
+                    </a>
+                    .
+                  </p>
+                </iframe>
               </object>
             </ResultsTable>
           </>
         );
+      }
       default:
         return null;
     }
