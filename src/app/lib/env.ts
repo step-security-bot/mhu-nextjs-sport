e337db9dd65c2fd44089ee0d42ae1e1d77807340
Martossy Alex <github@mail.martossy.hu>
2024-05-11 16:24:59 +0200

    Add firebase sdk

--- src/app/lib/env.ts
+++ src/app/lib/env.ts
@@ -18,17 +18,25 @@ export const env = createEnv({
     GOOGLE_CLIENT_SECRET: z.string().optional(),
     SIMPLELOGIN_CLIENT_ID: z.string(),
     SIMPLELOGIN_CLIENT_SECRET: z.string(),
+    FIREBASE_API_KEY: z.string(),
+    FIREBASE_AUTH_DOMAIN: z.string(),
+    FIREBASE_PROJECT_ID: z.string(),
+    FIREBASE_STORAGE_BUCKET: z.string(),
+    FIREBASE_MESSAGING_SENDER_ID: z.string(),
+    FIREBASE_APP_ID: z.string(),
   },
   client: {
     NEXT_PUBLIC_MAPS_API_KEY: z.string().optional(),
     NEXT_PUBLIC_SENTRY_DSN: z.string(),
     NEXT_PUBLIC_SENTRY_REPORT_URI: z.string(),
+    NEXT_PUBLIC_VAPID_KEY: z.string(),
   },
   // For Next.js >= 13.4.4, you need to destructure client variables:
   experimental__runtimeEnv: {
     NEXT_PUBLIC_MAPS_API_KEY: process.env['NEXT_PUBLIC_MAPS_API_KEY'],
     NEXT_PUBLIC_SENTRY_DSN: process.env['NEXT_PUBLIC_SENTRY_DSN'],
     NEXT_PUBLIC_SENTRY_REPORT_URI: process.env['NEXT_PUBLIC_SENTRY_REPORT_URI'],
+    NEXT_PUBLIC_VAPID_KEY: process.env['NEXT_PUBLIC_VAPID_KEY'],
   },
   extends: [vercel(), uploadthing()],
 });
