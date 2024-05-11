92865bf9db75bb86ce5436a75982e5331ad1e309
Alex Martossy <github@mail.martossy.hu>
2024-05-11 16:10:46 +0200

    Bump deps (#64)
    * Bump deps

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,11 @@ import { env } from '@/app/lib/env';
 export default {
   schema: './src/app/db/schema.ts',
   out: './drizzle',
-  driver: 'libsql',
+  driver: 'turso',
+  dialect: 'sqlite',
   dbCredentials: {
     url: env.TURSO_DATABASE_URL,
+    authToken: env.TURSO_AUTH_TOKEN,
   },
   strict: true,
   verbose: true,
